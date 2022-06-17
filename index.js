657af10d6405bad52b19265e21e9c7400a53723f
gabe19p <g19purselley@gmail.com>
2022-06-17 23:17:22 +0900

    http server set

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const express = require("express");
+const path = require("path");
+const app = express();
+
+app.get("/", (req, res) => {
+  //get method
+  res.sendFile(path.join(__dirname, "./views/index.html")); //respond with index
+});
+
+app.get("/grooming", (req, res) => {
+  //get method
+  res.sendFile(path.join(__dirname, "./views/grooming.html")); //respond with grooming
+});
+
+app.listen(3000);
