4560fb9c208d67cbc28c59720bef430ed3d05604
gabe19p <g19purselley@gmail.com>
2022-06-24 15:51:40 +0900

    u/d view engine; boarding + training rts

--- index.js
+++ index.js
@@ -11,19 +11,39 @@
 
 const express = require("express");
 const path = require("path");
+
 const app = express();
+
+//  set the view engine to html
+app.engine(".html", require("ejs").__express);
+
+//  assign the views path for my html
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
 
 //  tell express that the public directory holds the site assets
 app.use(express.static(__dirname + "/public")); // allows me to connect to my css + image files
 
 //  index route for landing
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/views/index.html")); //respond with index.html
+  res.render("index");
 });
+
 //  grooming route via anchor link or direct paste
 app.get("/grooming", (req, res) => {
-  res.sendFile(path.join(__dirname + "/views/grooming.html")); //respond with grooming
+  res.render("grooming"); //respond with grooming
 });
+
+//  boarding route via nav bar
+app.get('/boarding', (req, res) => {
+  res.render("boarding") //respond with boarding
+})
+
+//  training route via nav bar
+app.get('/training', (req, res) => {
+  res.render("training") //respond with boarding
+})
+
 //  incomplete route for the pages yet to be finished
 app.get("/incomplete", (req, res) => {
   res.send("Page Incomplete");
