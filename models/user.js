87fe3f2aed6f68d7fa3fac16c57daa4815d67ddc
gabe19p <g19purselley@gmail.com>
2022-07-03 12:27:04 +0900

    created user schema f/ db profiles

---
+++ models/user.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bCrypt-nodejs");
+
+const SALT_FACTOR = 10; //  the hash amount, as directed by the book
+const noop = function () {}; //  empty function for password hashing, also from the book
+
+//  setting up the user format
+const userSchema = mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  email: { type: String, required: true },
+});
+
+//  pre-save to hash the user's password
+userSchema.pre("save", function (done) {
+  let user = this; //  referencing the user
+
+  //  hash the password
+  bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
+    if (err) {
+      return done(err); //  error handlers
+    }
+    bcrypt.hash(user.password, salt, noop, function (err, hashedPassword) {
+      if (err) {
+        return done(err); //  error handlers
+      }
+      user.password = hashedPassword;
+      done();
+    });
+  });
+});
+
+//  checking the user's password
+userSchema.methods.checkPassword = function (guess, done) {
+  bcrypt.compare(guess, this.password, function (err, isMatch) {
+    done(err, isMatch);
+  });
+};
+
+userSchema.methods.name = function () {
+  return this.username;
+};
+
+//  assigning the Schema to an exportable module
+let User = mongoose.model("User", userSchema);
+module.exports = User;
